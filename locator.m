a61773a3a5db0d982f302bbbc52416f704a8cd3a
Utsav Mathur <utsavmat@usc.edu>
2015-02-14 19:08:22 -0800

    Create locator.m
    Our own locator class to store information about cities and their temperature, etc.

---
+++ locator.m
@@ -0,0 +1,51 @@
+//
+//  Location.m
+//  hack
+//
+//  Created by Antonis Papantoniou on 31/01/2015.
+//  Copyright (c) 2015 Antonis papantoniou. All rights reserved.
+//
+
+#import "Location.h"
+
+@interface Location()
+
+@property (strong, nonatomic) NSArray *temperature;
+@property (strong, nonatomic) NSArray *precipitation;
+
+
+@end
+
+@implementation Location
+
+- (id)initWithName:(NSString *)name temperature:(NSArray *)temperature precipitiation:(NSArray *)precipitation
+{
+    self = [super init];
+    
+    if (self) {
+        
+        self.name = name;
+        self.temperature = temperature;
+        self.precipitation = precipitation;
+    }
+    
+    return self;
+}
+
+
+
+- (NSString *)temperatureForDay:(NSInteger)day
+{
+    return (self.temperature[day]) ? self.temperature[day] : @"";
+}
+
+- (NSString *)precipitationForDay:(NSInteger)day
+{
+    return (self.precipitation[day]) ? self.precipitation[day] : @"";
+}
+- (NSString *)getName{
+    return(self.name);
+}
+
+
+@end
