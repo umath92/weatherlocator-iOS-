156d7c0234b266fc32b0c987415748fd69cdb8c1
Utsav Mathur <utsavmat@usc.edu>
2015-02-14 19:09:05 -0800

    Create secondViewController.h

---
+++ secondViewController.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+@interface secondViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *labelSceneTwo;
+@property (strong, nonatomic) NSString * labelText;
+@property (strong, nonatomic) IBOutlet UILabel *dateLab;
+
+@property (strong, nonatomic) IBOutlet UILabel *locLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tempLabel;
+@property (strong, nonatomic) IBOutlet UILabel *percLabel;
+@end
