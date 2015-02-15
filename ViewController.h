7a44cb20d23c62637417343d73983210f62799c0
Utsav Mathur <utsavmat@usc.edu>
2015-02-14 19:10:01 -0800

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+#import "secondViewController.h"
+
+@interface ViewController : UIViewController<UIAlertViewDelegate>
+
+
+@property (nonatomic, retain) IBOutlet UITextField *myTextField;
+
+
+
+@property (strong, nonatomic) IBOutlet UITextField *inputMonth;
+    
+@property (strong, nonatomic) IBOutlet UITextField *inputLocation;
+
+@end
+
+    
